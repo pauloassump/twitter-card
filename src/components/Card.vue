a6342a02762596187e0c00da10093ff74691656a
Paulo Henrique de Assumpção <paulo.henriquejcn@gmail.com>
2020-04-18 19:40:17 +0000

    minhas-alteracoes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="custom-card">
-    Teste
+    <div class="card-content">
+        <div class="media">
+            <div class="media-left">
+                <figure class="image is-48x48">
+                <img class="is-rounded" src="../images/paulo.jpeg" alt="Placeholder image">
+                </figure>
+            </div>
+            
+            <div class="media-content">
+                <p class="title is-5">Paulo H. de Assumpção</p>
+                <p class="subtitle is-6">28 anos, Joinville/SC</p>
+            </div>
+        </div>
+
+        <div class="content">
+            <br>
+            Desenvolvedor Frontend na Agapys Tecnologia. Cursando TADS na faculdade Anhanguera - 5ª fase.
+        </div>
+        <div class="content">
+            <span class="tag is-link">VueJs</span> <span class="tag is-link">Quasar Framework</span>
+        </div>
   </div>
+</div>
 </template>
 
 <script>
